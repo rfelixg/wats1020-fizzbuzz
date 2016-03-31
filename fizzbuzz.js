fdcb693d382abbb4a6f360b59f9c44791fe0bdc4
rfelixg <rfelixg@gmail.com>
2016-03-31 16:55:26 -0400

    Added comments

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,30 +1,38 @@
-// Place your FizzBuzz code here. 
+// FizzBuzz
+
+// Objective: count through numbers 1-20 and check if each one is divisible by 3, 5 or both 
+
+// for each number, perform the following check:
+
+// check if number divisible by 3
+// if so, check if number is also divisible by 5
+// if numbrer is divisible by 3 AND 5 --> print FizzBuzz
+// if number is only divisible by 3 --> print Fizz
+
+// check if number is divisible by 5
+// if number is divisibule by 5 --> print Buzz
+
+// check if number is NOT divisible by 3 or 5
+// if number is not divisible by 3 and 5 --> print number
+
+// ------------------------------------------------------------------------------------------------------------------------------------
 
 var rangeLimit = 100;
 
 for (i = 1; i <= rangeLimit; i++){
-  // for each number, perform the following check:
-
-  // check if number divisible by 3
   if (i % 3 === 0) {
-    // % modulus checks if it's an even division. In this case if it's an even division that means the number is in fact divisible by 3
     
-  // if so, check if number is also divisible by 5
     if (i % 5 === 0) {
-        // if numbrer is divisible by 3 AND 5 --> print FizzBuzz
       console.log('FizzBuzz'); 
     } else {
-        // if number is only divisible by 3 --> print Fizz
       console.log('Fizz');
     }
   }
- 
-  // check if number is divisible by 5
+  
   else if (i % 5 === 0) {
-      // if number is divisibule by 5 --> print Buzz
     console.log('Buzz');
   } else {
-  // if number is not divisible by 3 and 5 --> print number
+    
     console.log(i);
   }
 }
